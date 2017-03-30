eca85ef08bc4962e981a63fc93dd5009a6bd111d
zzy <492510851@qq.com>
2017-03-30 21:46:54 +0800

    服务实现登录页面

---
+++ services/fonur/fonour/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fonour.MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
