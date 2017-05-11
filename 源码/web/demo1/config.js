4954b4ac1738dbf378131c581969192eadc521f4
zhangziyi123 <492510851@qq.com>
2017-05-12 00:09:57 +0800

    Signed-off-by: zhangziyi123 <492510851@qq.com>

---
+++ 源码/web/demo1/config.js
@@ -0,0 +1,12 @@
+/**
+ * @author: zhang@glondon.com
+ * @description:
+ * @Date: 2017/5/11 22:34
+ */
+define([],function(){
+    var config={
+        url:["getUrl"]
+
+    }
+    return config;
+})
---
+++ 源码/web/demo1/core/app.js
@@ -0,0 +1,17 @@
+/**
+ * @author: zhang@glondon.com
+ * @description:
+ * @Date: 2017/5/11 22:33
+ */
+ require(["js/config"],function(config){
+     var app={
+
+         init:function () {
+             alert("123");
+         }
+
+     }
+     app.init();
+   //  return app;
+
+ })
